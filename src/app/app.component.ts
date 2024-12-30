8d4adbc23159264b4635bcc8b817ec9b0967a062
Lukasz Rzeszotarski <softinnotec@outlook.com>
2024-12-30 22:00:28 +0100

    chessboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {SoftinnotecChessboardComponent} from "./softinnotec-chessboard/softinnotec-chessboard.component";
+import {BannerComponent} from "./banner/banner.component";
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
     selector: 'app-root',
-    imports: [SoftinnotecChessboardComponent],
+    imports: [SoftinnotecChessboardComponent, BannerComponent, RouterOutlet, MatCardModule],
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.sass'
