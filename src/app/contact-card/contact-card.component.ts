8d07a0c7701720d85cfe31b9ac865fc226a48e82
Lukasz Rzeszotarski <softinnotec@outlook.com>
2024-12-31 23:49:51 +0100

    chessboard

--- src/app/contact-card/contact-card.component.ts
+++ src/app/contact-card/contact-card.component.ts
@@ -2,13 +2,17 @@ import { Component } from '@angular/core';
 import {CardComponent} from "../card/card.component";
 import {VatIDFormatterPipe} from "../vat-idformatter.pipe";
 import {PhoneFormatterPipe} from "../phone-formatter.pipe";
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-contact-card',
   imports: [
     CardComponent,
     VatIDFormatterPipe,
-    PhoneFormatterPipe
+    PhoneFormatterPipe,
+    MatButtonModule,
+    MatIconModule,
   ],
   standalone: true,
   templateUrl: './contact-card.component.html',
