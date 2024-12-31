ebcb1a57654b9d49a6964aa975f498c10eae68f4
Lukasz Rzeszotarski <softinnotec@outlook.com>
2024-12-31 16:52:54 +0100

    chessboard

--- src/app/card-grid/card-grid.component.ts
+++ src/app/card-grid/card-grid.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import {CardComponent} from "../card/card.component";
+import {AboutCardComponent} from "../about-card/about-card.component";
 
 @Component({
   selector: 'app-card-grid',
-  imports: [CardComponent],
+  imports: [CardComponent, AboutCardComponent],
   standalone: true,
   templateUrl: './card-grid.component.html',
   styleUrl: './card-grid.component.sass'
