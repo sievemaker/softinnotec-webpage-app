7025e039171aff92ad8bdce5b19419c10b379395
Lukasz Rzeszotarski <softinnotec@outlook.com>
2025-01-18 14:49:55 +0100

    consent cookie policy etc.

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
+import {NgClass} from "@angular/common";
 
 
 @Component({
   selector: 'app-footer',
-  imports: [MatTabsModule, MatIconModule],
+  imports: [MatTabsModule, MatIconModule, NgClass],
   templateUrl: './footer.component.html',
   standalone: true,
   styleUrl: './footer.component.sass'
 })
 export class FooterComponent {
+  isModalVisible = false;
 
+  toggleModal(): void {
+    this.isModalVisible = !this.isModalVisible;
+  }
 }
